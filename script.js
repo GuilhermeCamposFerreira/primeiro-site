a739ee4939a7b202c0c220f51c250396b787db63
GuilhermeCamposFerreira <guicafe2#@usp.br>
2023-11-13 11:21:43 -0300

    adiciona readme

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ const img = document.querySelector("#profile img")
 // substituir a imagem
 if (html.classList.contains ("light")) {
 // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/eu_ela.jpg")
+    img.setAttribute("src", "./assets/Eu_ela-dia.jpeg")
 } else {
 // set tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/eu_ela.jpg")
 }
 }
